040045a3ef4de06aac449a16ff3f3ca470ff0b7d
Ioan Davies <ioandavies067@gmail.com>
2021-11-23 16:33:08 +0000

    Add multiple client support for WebSockets

--- leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/WebSocketServer.java
+++ leshan-server-demo/src/main/java/org/eclipse/leshan/server/demo/servlet/WebSocketServer.java
@@ -1,10 +1,13 @@
 package org.eclipse.leshan.server.demo.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,16 +17,29 @@ public class WebSocketServer {
 	private Socket socket;
 	
 	public WebSocketServer() throws IOException {
-		serverSocket = new ServerSocket(4999);
 		Thread connectionThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					socket = serverSocket.accept();
-			        InputStream input = socket.getInputStream();
-			        OutputStream output = socket.getOutputStream();
-					
-					System.out.println(input.read());
+					Socket socket=null;
+					ServerSocket serverSocket=null;
+					System.out.println("Server Listening......");
+					serverSocket = new ServerSocket(4999);
+					//TODO: Close socket when data is recieved successfully
+					while(true){
+						try{
+							socket= serverSocket.accept();
+							System.out.println("connection Established");
+							ServerThread serverThread = new ServerThread(socket);
+							serverThread.start();
+
+						}
+
+						catch(Exception e){
+							e.printStackTrace();
+
+						}
+					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -35,5 +51,46 @@ public class WebSocketServer {
 	public void close() throws IOException {
 		serverSocket.close();
 	}
+	
+	
 
 }
+
+
+class ServerThread extends Thread{  
+
+	String line = null;
+	BufferedReader inputStream = null;
+	PrintWriter outputStream = null;
+	Socket socket = null;
+
+	public ServerThread(Socket socket){
+		this.socket = socket;
+	}
+
+	public void run() {
+		try{
+			inputStream= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			outputStream=new PrintWriter(socket.getOutputStream());
+
+		}catch(IOException e){
+			System.out.println("IO error in server thread");
+		}
+        while (true) {
+            try {
+                line = inputStream.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    socket.close();
+                    return;
+                } else {
+                	System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+	}
+}
+
+
