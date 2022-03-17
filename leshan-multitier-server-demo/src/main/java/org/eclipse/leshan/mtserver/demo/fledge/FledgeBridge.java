b56ff09f01a74bdc9c48344346c7d3ad81e69eab
IoanEOD <ioandavies067@gmail.com>
2022-03-17 16:14:26 +0000

    Add functionality for object observe to Fledge

--- leshan-multitier-server-demo/src/main/java/org/eclipse/leshan/mtserver/demo/fledge/FledgeBridge.java
+++ leshan-multitier-server-demo/src/main/java/org/eclipse/leshan/mtserver/demo/fledge/FledgeBridge.java
@@ -9,6 +9,7 @@ import java.net.http.HttpResponse;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 import java.util.TimeZone;
 
 import javax.json.Json;
@@ -16,6 +17,9 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
+import org.eclipse.leshan.core.node.LwM2mObjectInstance;
+import org.eclipse.leshan.core.node.LwM2mPath;
+import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.LwM2mSingleResource;
 import org.eclipse.leshan.core.observation.CompositeObservation;
 import org.eclipse.leshan.core.observation.SingleObservation;
@@ -33,39 +37,38 @@ public class FledgeBridge {
                                 ObserveResponse response) {
 
                         String time = currentTimeAsIso();
+
+                        LwM2mPath path = observation.getPath();
+                        // TODO: Iterate over all id's and add to json payload
+
+                        String objectPath = registration.getEndpoint() + "/" + path.getObjectId() + "/"
+                                        + path.getObjectInstanceId();
+
+                        JsonObjectBuilder readingsBuilder = Json.createObjectBuilder();
+
                         
-                        //TODO: Iterate over all id's and add to json payload
-                        String path = registration.getEndpoint() + observation.getPath().toString();
+                        if (observation.getPath().isObjectInstance()) {
+                                // Observation for entire object
+                                LwM2mObjectInstance lwM2mObjectInstance = (LwM2mObjectInstance) response.getContent();
+                                Map<Integer, LwM2mResource> resources = lwM2mObjectInstance.getResources();
 
-                        LwM2mSingleResource node = (LwM2mSingleResource) response.getContent();
-
-                        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder()
-                                        .add("timestamp", time
-                                        )
-                                        .add("asset", path);
-
-                        switch (node.getValue().getClass().getSimpleName()) {
-                                case "String":
-                                        jsonBuilder.add("readings", Json.createObjectBuilder()
-                                                        .add("value", node.getValue().toString()));
-                                        break;
-                                case "Double":
-                                        jsonBuilder.add("readings", Json.createObjectBuilder()
-                                                        .add("value", (Double) node.getValue()));
-                                        break;
-                                case "Integer":
-                                        jsonBuilder.add("readings", Json.createObjectBuilder()
-                                                        .add("value", (Integer) node.getValue()));
-                                        break;
-                                // TODO: Implement other data types
-                                default:
-                                        jsonBuilder.add("readings", Json.createObjectBuilder()
-                                                        .add("value", node.getValue().toString()));
-                                        break;
+                                for (Map.Entry<Integer, LwM2mResource> resource : resources.entrySet()) {
+                                        addValueToBuilder(readingsBuilder, Integer.toString(resource.getValue().getId()),
+                                        resource.getValue().getValue());
+                                    }
+                        } else if (observation.getPath().isResource()) {
+                                // Observation for single resource of an object
+                                LwM2mResource resource = (LwM2mResource) response.getContent();
+                                addValueToBuilder(readingsBuilder, Integer.toString(resource.getId()), resource.getValue());
                         }
 
-                        JsonObject jsonObject = jsonBuilder.build();
-                        String jsonPayload = "[" + jsonObject.toString() + "]";
+                        JsonObjectBuilder payloadBuilder = Json.createObjectBuilder()
+                                        .add("timestamp", time)
+                                        .add("asset", objectPath);
+
+                        payloadBuilder.add("readings", readingsBuilder);
+
+                        String jsonPayload = "[" + payloadBuilder.build().toString() + "]";
 
                         HttpClient client = HttpClient.newHttpClient();
                         HttpRequest httpRequest = HttpRequest.newBuilder()
@@ -87,14 +90,12 @@ public class FledgeBridge {
                                 e.printStackTrace();
                         }
 
-                        String y = "f";
-
                 };
 
                 @Override
                 public void onResponse(CompositeObservation observation, Registration registration,
                                 ObserveCompositeResponse response) {
-                        
+
                         System.out.println("x");
                         // TODO Composite HTTP Post to Fledge
                 };
@@ -124,5 +125,20 @@ public class FledgeBridge {
                 DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
                 df.setTimeZone(tz);
                 return df.format(new Date());
+        }
+
+        private void addValueToBuilder(JsonObjectBuilder builder, String asset, Object value) {
+                switch (value.getClass().getSimpleName()) {
+                        case "Double":
+                                builder.add(asset, (Double) value);
+                                break;
+                        case "Integer":
+                                builder.add(asset, (Integer) value);
+                                break;
+                        // TODO: Implement other data types (Supported by Fledge)
+                        default:
+                                builder.add(asset, value.toString());
+                                break;
+                }
         }
 }
